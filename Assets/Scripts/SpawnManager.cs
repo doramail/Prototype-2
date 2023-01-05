358b21ef4fadaaa719173ac00aa8a2942b49fd4c
doramail <doramail@users.noreply.github.com>
2023-01-05 13:53:21 +0100

    Suite 2.3
    In progress

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManage : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
 
